78e8a297c751022c1d87b7bad39c89a26e9a64a5
llulima23-lang <llu.lima23@gmail.com>
2026-05-12 09:27:20 -0300

    Add files via upload

---
+++ users.js
@@ -0,0 +1,27 @@
+const USERS = [
+    { name: "Juliana Von", password: "4kL9p2" },
+    { name: "Jorge Neto", password: "J3s7Gf" },
+    { name: "Allan Dourado", password: "Q9r1Wz" },
+    { name: "Carcus Vinicius", password: "A5d8kL" },
+    { name: "Adilline Bezerra", password: "t6M0pY" },
+    { name: "Adriana Nascimento", password: "H7n2Xw" },
+    { name: "Vanessa Sobrinho", password: "9bV4mD" },
+    { name: "Vanessa Brito", password: "2yT6hP" },
+    { name: "Paula Targino", password: "5rN1fB" },
+    { name: "Miriam Torres", password: "x8C3vN" },
+    { name: "Karyanne Oliveira", password: "3mR8vQ" },
+    { name: "Emerson Sousa", password: "P1s6Kj" },
+    { name: "Tatiane Vasconcelos", password: "G4n9Xc" },
+    { name: "Mikaelle Sousa", password: "7kW2mS" },
+    { name: "Thais Freitas", password: "9hJ2qR" },
+    { name: "Patricia Furtado", password: "B5t1Zy" },
+    { name: "Mayara Souza", password: "8fL2Nc" },
+    { name: "Raiza Brandão", password: "5vM3pQ" },
+    { name: "Debora Vidal", password: "Z9r4jD" },
+    { name: "Adriene Jeane", password: "1pT7vX" },
+    { name: "Assistente Tim", password: "M4k9Rz" },
+    { name: "Assistente Enel", password: "2fH8vX" },
+    { name: "Luciana Lima", password: "7bW1pS", isAdmin: true }
+];
+
+export default USERS;
